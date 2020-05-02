2dd432efabe2540d6a811ae9bbb39275241312b4
CommanderDerpy <max.gewers@gewers.com>
2020-05-01 22:37:42 -0600

    initial commit

---
+++ src/components/HelloWorld.native.vue
@@ -0,0 +1,18 @@
+<template>
+  <GridLayout rows="auto, auto">
+    <Label class="message" :text="msg" row="0" horizontalAlignment="center" />
+    <!-- copy-webpack-plugin copies asset from src/assets to project output/build directory /assets -->
+    <Image src="~/components/icon.png" row="1" class="m-40" />
+  </GridLayout>
+</template>
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component({
+  name: "HelloWorld"
+})
+export default class HelloWorld extends Vue {
+  @Prop(String) private msg!: string;
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="message">{{ msg }}</div>
+    <img src="~/components/icon.png" alt="logo" />
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component({
+  name: "HelloWorld"
+})
+export default class HelloWorld extends Vue {
+  @Prop(String) private msg!: string;
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
